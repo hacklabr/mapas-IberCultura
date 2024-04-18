031c2199e08431f5e3b257ddd56bc2a922addcab
Welington De Lima Olegario <wolegario49@gmail.com>
2024-04-18 14:28:50 -0300

    Faz a alteração da logo padrão

---
+++ dev/config.d/logo.php
@@ -0,0 +1,27 @@
+<?php
+
+return [
+    /* Primeira linha do logo configurável */
+    'logo.title' => 'Mapa Cultural',
+
+    /* Segunda linha do logo configurável */
+    'logo.subtitle' => 'da IBERCULTURAVIVA',
+
+    /* Cores da logo */
+    'logo.colors' => [
+        "var(--mc-primary-300)",
+        "var(--mc-primary-500)",
+        "var(--mc-secondary-300)",
+        "var(--mc-secondary-500)",
+    ],
+
+    /* 
+    Define o nome do asset da imagem da logo do site - Substituirá a logo padrão
+
+    ex: `img/meu-mapa-logo.jpg` (pasta assets/img/meu-mapa-logo.jpg do tema) 
+    */
+    'logo.image' => 'img/logo-site.svg',
+
+    /* Esconde o título e subtitulo */
+    'logo.hideLabel' => true,
+];
