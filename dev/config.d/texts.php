ab45034ee82605721fd7d78f1495289d91d58624
Welington De Lima Olegario <wolegario49@gmail.com>
2024-04-24 16:02:34 -0300

    Cria configuração de texto da home Ref.: #1

---
+++ dev/config.d/texts.php
@@ -0,0 +1,15 @@
+<?php
+use \MapasCulturais\i;
+
+return [
+    "text:home-header.title" => i::__("Bem-vind@!"),
+    "text:home-header.description" => i::__("O Mapa IberCultura Viva é a plataforma livre, gratuita e colaborativa de mapeamento do programa de cooperação IberCultura Viva. Neste espaço, representantes de organizações culturais comunitárias e povos originários podem se registrar como agentes culturais, difundir eventos, cadastrar projetos e inscrever-se nos editais e concursos publicados. <br> A informação coletada pelo Mapa IberCultura Viva será utilizada para a construção/consolidação de indicadores culturais que fortalecerão os sistemas de informação cultural dos países membros do programa (Argentina, Brasil, Chile, Colômbia, Costa Rica, El Salvador, Equador, Espanha, México, Paraguai, Peru e Uruguai)."),
+    "text:home-opportunities.description" => i::__("Cadastre-se, participe de editais e oportunidades e concorra aos benefícios."),
+    "text:home-entities.events" => i::__("Você pode pesquisar eventos culturais nos campos de busca do seu interesse que ajudam na  precisão de sua pesquisa. Como usuário cadastrado, você pode incluir seus eventos na plataforma e divulgá-los gratuitamente."),
+    "text:home-entities.agents" => i::__("Neste espaço, estão registrados artistas, gestores e produtores; uma rede de atores envolvidos na cena cultural da região. Você pode cadastrar um ou mais agentes (grupos, coletivos, bandas, instituições, empresas, associações, cooperativas e etc)."),
+    "text:home-entities.projects" => i::__("Aqui você encontra leis de fomento, mostras, convocatórias, chamadas públicas e editais criados, além de diversas iniciativas cadastradas pelos usuários da plataforma."),
+    "text:home-feature.description" => i::__("Confira os últimos destaques de cada uma das áreas."),
+    "text:home-register.description" => i::__("Contribua com a plataforma livre e interativa de mapeamento do cenário cultural e instrumento de governança digital no aprimoramento da gestão pública, dos mecanismos de participação e da democratização do acesso às políticas culturais promovidas."),
+    'text:home-developers.description' => 'Existem algumas maneiras de desenvolvedores interagirem com o Mapas Culturais. A primeira é através da nossa API. Com ela você pode acessar os dados públicos no nosso banco de dados e utilizá-los para desenvolver aplicações externas. Além disso, o Mapas Culturais é construído a partir do sofware livre Mapas Culturais, criado em parceria com o Instituto TIM, e você pode contribuir para o seu desenvolvimento através do GitHub.',
+    "text:home-map.description" => i::__("Os agentes, os espaços e os eventos cadastrados contam com a geolocalização de seus endereços, encontre-os aqui:"),
+];
