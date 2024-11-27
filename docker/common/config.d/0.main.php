2427774a552811393274cc681c2a7a230f190fef
erleibiazzio <erleibiazzio@gmail.com>
2024-11-27 13:04:29 -0300

    Corrige para definição do thema em produção

--- docker/common/config.d/0.main.php
+++ docker/common/config.d/0.main.php
@@ -1,13 +1,12 @@
 <?php 
 
 return [
-    'themes.active' => 'IberCulturaViva',
     
     'app.siteName' => 'Mapa IberCultura Viva',
     'app.siteDescription' => 'O Mapas Culturais é uma plataforma colaborativa que reúne informações sobre agentes, espaços, eventos, projetos culturais e oportunidades',
     
     // Define o tema ativo no site principal. Deve ser informado o namespace do tema e neste deve existir uma classe Theme.
-    'themes.active' => 'MapasCulturais\Themes\BaseV2',
+    'themes.active' => 'IberCulturaViva',
 
     // Ids dos selos verificadores. Para utilizar múltiplos selos informe os ids separados por vírgula.
     'app.verifiedSealsIds' => '1', 
