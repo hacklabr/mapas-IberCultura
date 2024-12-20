57a1e80a6f855eb6eae6e6d59819ac8f79c1eb96
Welington Olegario <wolegario49@gmail.com>
2024-12-20 14:39:40 -0300

    Ajuste nas imagens

---
+++ docker/common/config.d/module.home.php
@@ -0,0 +1,12 @@
+<?php
+return [
+    "module.home" => [
+        'home-header' => env('MODULE_HOME_IMAGE_HEADER','img/home/home-header/home-header2.jpg'),
+        'home-agents' => env('MODULE_HOME_IMAGE_AGENT','img/home/home-entities/home--agents.jpg'),
+        'home-events' => env('MODULE_HOME_IMAGE_EVENTS','img/home/home-entities/home--events.jpg'),
+        'home-opportunities' => env('MODULE_HOME_IMAGE_OPPORTUNITIES','img/home/home-entities/home--opportunities.jpg'),
+        'home-projects' => env('MODULE_HOME_IMAGE_PROJECTS','img/home/home-entities/home--projects.jpg'),
+        'home-spaces' => env('MODULE_HOME_IMAGE_SPACES','img/home/home-entities/home--spaces.jpg'),
+        'home-register' => env('MODULE_HOME_IMAGE_REGISTER','img/home/home-register/background.jpg'),      
+    ]    
+];
