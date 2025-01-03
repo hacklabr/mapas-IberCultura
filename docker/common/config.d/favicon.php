179dbf4faa1c98cf9ae52ae05e835b141d8eaadd
Welington De Lima Olegario <wolegario49@gmail.com>
2025-01-03 10:39:13 -0300

    Criação dos favicons

---
+++ docker/common/config.d/favicon.php
@@ -0,0 +1,6 @@
+<?php
+return [
+    'favicon.ico' => env('FAVICON_IMAGE_ICO', 'img/favicon.ico'),
+    'favicon.svg' => env('FAVICON_IMAGE_SVG', 'img/favicon.svg'),
+    'favicon.180' => env('FAVICON_IMAGE_180', 'img/favicon-180x180.png'),
+];
